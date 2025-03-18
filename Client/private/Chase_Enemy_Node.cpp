dbb47c003ca6701cc79b8d20741407f8e6fe2c6e
Hero <hanwoong2042@naver.com>
2025-03-18 21:36:54 +0900

    뇌한테 몸체 빼기

--- Client/private/Chase_Enemy_Node.cpp
+++ Client/private/Chase_Enemy_Node.cpp
@@ -2,27 +2,17 @@
 #include "GameInstance.h"
 #include "HeroEnemy.h"
 
-CChase_Enemy_Node::CChase_Enemy_Node(CHeroEnemy* pEnemy)
-	:m_pHeroEnemy(pEnemy)
-{
-}
-
-CChase_Enemy_Node::~CChase_Enemy_Node()
-{
-}
-
 STATUS CChase_Enemy_Node::Excute(CGameObject* _Obj, _float _fTimeDelta)
 {
-	if (!m_pHeroEnemy->Get_Target())
-		return STATUS::FAIL;
+	CHeroEnemy* pHeroEnemy = static_cast<CHeroEnemy*>(_Obj);
 
-	m_pHeroEnemy ->Get_Transform()->LookAt(m_pHeroEnemy->Get_Target_Transform()->Get_State(CTransform::STATE_POSITION));
-	m_pHeroEnemy->Get_Transform()->Chase(m_pHeroEnemy->Get_Target_Transform()->Get_State(CTransform::STATE_POSITION), _fTimeDelta, 10.f);
+	if (!pHeroEnemy->Get_Target())
+	{
+		return STATUS::FAIL;
+	}
+
+	pHeroEnemy->Get_Transform()->LookAt(pHeroEnemy->Get_Target_Transform()->Get_State(CTransform::STATE_POSITION));
+	pHeroEnemy->Get_Transform()->Chase(pHeroEnemy->Get_Target_Transform()->Get_State(CTransform::STATE_POSITION), _fTimeDelta, 10.f);
 
 	return STATUS::SUCCESS;
-}
-
-CChase_Enemy_Node* CChase_Enemy_Node::Create(CHeroEnemy* pEnemy)
-{
-	return new CChase_Enemy_Node(pEnemy);
 }
--- Client/public/Chase_Enemy_Node.h
+++ Client/public/Chase_Enemy_Node.h
@@ -8,17 +8,7 @@ class CHeroEnemy;
 
 class CChase_Enemy_Node : public CNode
 {
-private:
-	CChase_Enemy_Node() = default;
-	CChase_Enemy_Node(CHeroEnemy* pEnemy);
-	virtual ~CChase_Enemy_Node();
-
 public:
 	STATUS Excute(CGameObject* _Obj, _float _fTimeDelta) override;
-
-private:
-	CHeroEnemy* m_pHeroEnemy{ nullptr };
-public:
-	static CChase_Enemy_Node* Create(CHeroEnemy* pEnemy);
 };
 END
