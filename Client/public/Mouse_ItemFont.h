ee7246276d7c740897abbca5819ba7c27384b68e
gusdn6134 <gusdn6134@naver.com>
2025-04-03 18:41:57 +0900

    인벤토리 버그 수정 및 아이템 개수 쉐이더 적용
    인벤토리 버그 수정 및 아이템 개수 쉐이더 적용

--- Client/public/Mouse_ItemFont.h
+++ Client/public/Mouse_ItemFont.h
@@ -30,16 +30,16 @@ public:
 	_bool		Get_Check() { return m_bCheck; }
 
 public:
-	void Set_Check(_bool _bCheck) { m_bCheck = _bCheck; }
-	void Set_ItemName(ITEMNAME _ItemName) { m_ItemName = _ItemName; }
-	void Set_ItemPos(UIOBJECT_DESC _Desc) { Desc = _Desc; }
-	void Set_ItemFont_Tens(_int _TextureTens) { m_Texture_TensNum = _TextureTens; }
-	void Set_ItemFont_Ones(_int _TextureOnes) { m_Texture_OnesNum = _TextureOnes; }
+	void Set_Check(_bool _bCheck)				{ m_bCheck = _bCheck; }
+	void Set_ItemName(ITEMNAME _ItemName)		{ m_ItemName = _ItemName; }
+	void Set_ItemPos(UIOBJECT_DESC _Desc)		{ Desc = _Desc; }
+	void Set_ItemFont_Tens(_int _TextureTens)	{ m_Texture_TensNum = _TextureTens; }
+	void Set_ItemFont_Ones(_int _TextureOnes)	{ m_Texture_OnesNum = _TextureOnes; }
 
 	
 
 private:
-	HRESULT RendeTexture(CTexture* pTextureCom, _int _TextureNun, _float _fX, _float _fY);
+	HRESULT RendeTexture(CTexture* pTextureCom, _int _TextureNum, _float _fX, _float _fY);
 	HRESULT Ready_Components();
 	HRESULT	SetUp_RenderState();
 	HRESULT	Reset_RenderState();
@@ -48,9 +48,10 @@ private:
 	UIOBJECT_DESC Desc{};
 
 private:
-	CTexture* m_pTextureCom = { nullptr };
-	CTransform* m_pTransformCom = { nullptr };
-	CVIBuffer_Rect* m_pVIBufferCom = { nullptr };
+	CTexture*			m_pTextureCom = { nullptr };
+	CTransform*			m_pTransformCom = { nullptr };
+	CVIBuffer_Rect*		m_pVIBufferCom = { nullptr };
+	CShader*			m_pShaderCom = { nullptr };
 
 private:
 	_bool		m_bCheck = { false };
